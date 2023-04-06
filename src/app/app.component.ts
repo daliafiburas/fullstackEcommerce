4fcb9bb579860f94ea6799c5edd438aed841a7c4
Dalia <dalia.dlugosz96@gmail.com>
2023-04-06 16:13:40 +0200

    saluti e conf

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'alphashop';
+
+  saluti : string = "Benvenuti in alphashop";
+
+  bollini : number = 1500;
+
+
+}
